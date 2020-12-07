19a33a4f3bc66baee47d28899b341dee0959ec9d
arkumish <arpit.cse24@gmail.com>
2020-12-07 23:20:41 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+
+const app = express()
+const port = 7700;
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
+   
+app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, './')));
+
+app.listen(port, () => console.log(`App listening on port ${port}!`))
